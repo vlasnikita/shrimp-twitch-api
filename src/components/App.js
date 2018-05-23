a58e9add47f53e1458d1dd8eab07b7026b6ef766
vlasnikita <vlasenkonikita@gmail.com>
2018-05-23 18:35:01 +0300

    bug fixes

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,6 @@ import React, {Component} from 'react'
 import SearchForm from './SearchForm'
 
 class App extends Component {
-    static childContextTypes = {
-        username: PropTypes.string
-    }
-
-    getChildContext() {
-        return {
-            username: this.state.user
-        }
-    }
-
-    state = {
-        user: ''
-    }
-
-    handleUserChange = user => this.setState({ user })
 
     render() {
         return (
