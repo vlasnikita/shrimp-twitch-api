ee95eeefe38284e35ddb7f902bf7067053494325
vlasnikita <vlasenkonikita@gmail.com>
2018-05-23 18:10:19 +0300

    added port

--- simple_api/server.js
+++ simple_api/server.js
@@ -9,7 +9,7 @@ var app = express();
 app.use(bodyParser.json());
 app.use('/api', api)
 
-app.listen(port, 'localhost', function (err) {
+app.listen(process.env.PORT || port, function (err) {
     if (err) {
         console.log(err);
         return;
