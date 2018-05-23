e777755901b6b91d4b5edd59ccc80fb82603b7b8
vlasnikita <vlasenkonikita@gmail.com>
2018-05-23 18:26:50 +0300

    deleted redux

--- src/middlewares/logger.js
+++
@@ -1,6 +0,0 @@
-export default store => next => action => {
-    console.log('---', 'state before', store.getState())
-    console.log('---', 'dispatching', action)
-    next(action)
-    console.log('---', 'state after', store.getState())
-}
